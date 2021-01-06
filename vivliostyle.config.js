36e91952c5e1d659c26528fe276371f2677d2cf8
takanorip <takanori0170@gmail.com>
2021-01-06 22:31:10 +0900

    add config

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  title: 'dokusyobiyori', // populated into `manifest.json`, default to `title` of the first entry or `name` in `package.json`.
+  title: 'デザイナーが知るべきWeb標準のこと', // populated into `manifest.json`, default to `title` of the first entry or `name` in `package.json`.
   author: 'takanorip <takanori0170@gmail.com>', // default to `author` in `package.json` or undefined.
-  // language: 'ja', // default to `en`.
-  // size: 'A4', // paper size.
+  language: 'ja', // default to `en`.
+  size: 'JIS-B5', // paper size.
   theme: '@vivliostyle/theme-techbook', // .css or local dir or npm package. default to undefined.
   // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
   entry: [
-    'manuscript.md', // `title` is automatically guessed from the file (frontmatter > first heading).
+    '1.md',
+    '2.md',
+    '3.md'
     // {
     //   path: 'epigraph.md',
     //   title: 'Epigraph', // title can be overwritten (entry > file),
@@ -15,6 +17,6 @@ module.exports = {
     // 'glossary.html', // html can be passed.
   ], // `entry` can be `string` or `object` if there's only single markdown file.
   // toc: true, // whether generate and include toc.html or not (does not affect manifest.json), default to `true`. if `string` given, use it as a custom toc.html.
-  // cover: './cover.png', // cover image. default to undefined.
+  cover: './cover.png', // cover image. default to undefined.
   // workDir: './dist', // default to `.vivliostyle`.
 }
